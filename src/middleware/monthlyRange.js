93d335f5920f92f5e697b4f79b88383fa68b7af7
Lucky Dewa Satria <luckydewa123@gmail.com>
2022-01-10 21:47:11 +0700

    feat: add range middleware

---
+++ src/middleware/monthlyRange.js
@@ -0,0 +1,56 @@
+const { check } = require('express-validator');
+
+const dateChecker = (value) => {
+  const dateFormatRegex = /^\d{4}\.\d{2}/;
+  if (!dateFormatRegex.test(value)) return false;
+  return true;
+};
+
+const isYearQueryEqualYearParam = (yearQuery, yearParam) => {
+  if (yearQuery == yearParam) return true;
+  return false;
+};
+const isMonthQueryEqualMonthParam = (MonthQuery, MonthParam) => {
+  if (MonthQuery == MonthParam) return true;
+  return false;
+};
+
+const since = check('since').custom((value, { req }) => {
+  if (!value) return true;
+  if (!dateChecker(value)) throw new Error('Invalid date format');
+
+  const [yearQuery, monthQuery, dateQuery] = value.split('.');
+
+  const { year, month, date } = req.params;
+
+  if (year && !isYearQueryEqualYearParam(yearQuery, year)) {
+    throw new Error('Year query is not equal year param');
+  }
+
+  if (month && !isMonthQueryEqualMonthParam(monthQuery, month)) {
+    throw new Error('Month query is not equal month param');
+  }
+
+  return true;
+});
+
+const upto = check('upto').custom((value, { req }) => {
+  if (!value) return true;
+  if (!dateChecker(value)) throw new Error('Invalid date format');
+
+  const [yearQuery, monthQuery, dateQuery] = value.split('.');
+
+  const { year, month, date } = req.params;
+
+  if (year && !isYearQueryEqualYearParam(yearQuery, year)) {
+    throw new Error('Year query is not equal year param');
+  }
+
+  if (month && !isMonthQueryEqualMonthParam(monthQuery, month)) {
+    throw new Error('Month query is not equal month param');
+  }
+
+  return true;
+});
+
+module.exports = { since, upto };
