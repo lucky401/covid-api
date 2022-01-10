3c3e5c844adf85893eace1ca8fc788fce2ff8d87
Lucky Dewa Satria <luckydewa123@gmail.com>
2022-01-10 21:46:45 +0700

    feat: middleware

--- src/utils/response.js
+++ src/utils/response.js
@@ -1,10 +1,10 @@
 function errorRes(res, err, errMsg = 'failed operation', statusCode = 500) {
   console.error('ERROR:', err);
-  return res.status(statusCode).json({ success: false, error: errMsg });
+  return res.status(statusCode).json({ ok: false, error: errMsg });
 }
 
 function successRes(res, data = {}, statusCode = 200) {
-  return res.status(statusCode).json({ success: true, data });
+  return res.status(statusCode).json({ ok: true, data, message: 'success' });
 }
 
 function errData(res, errMsg = 'failed operation') {
